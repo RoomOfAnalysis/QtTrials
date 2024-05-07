4666b441e2acef9ebf0652ae7b883b38a1eddf37
Harold <pxduharold@126.com>
2024-05-07 15:32:23 +0800

    x try with waiting spinner

--- QtAnimation/MainWindow.cpp
+++ QtAnimation/MainWindow.cpp
@@ -3,6 +3,8 @@
 
 #include <QTimer>
 
+#include "waitingspinnerwidget.h"
+
 MainWindow::MainWindow(QWidget* parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
@@ -34,17 +36,24 @@ void MainWindow::setup_connections()
         });
     });
     connect(ui->actionpage2, &QAction::triggered, this, [this] {
-        auto* label = new QLabel(ui->stackedWidget->currentWidget());
-        label->setStyleSheet("background: white");
-        label->setText("loading 2");
-        label->resize(label->parentWidget()->size());
-        label->show();
-        label->activateWindow();
-        label->raise();
+        auto* spinner = new WaitingSpinnerWidget(ui->stackedWidget->currentWidget());
+        spinner->setRoundness(70.0);
+        spinner->setMinimumTrailOpacity(15.0);
+        spinner->setTrailFadePercentage(70.0);
+        spinner->setNumberOfLines(12);
+        spinner->setLineLength(10);
+        spinner->setLineWidth(5);
+        spinner->setInnerRadius(10);
+        spinner->setRevolutionsPerSecond(1);
+        spinner->setColor(QColor(81, 4, 71));
+        spinner->setText("loading 2");
+        spinner->setTextColor(Qt::cyan);
+        spinner->start();
 
-        QTimer::singleShot(1000, [this, label]() {
-            delete label;
-            ui->stackedWidget->setCurrentIndex(0);
+        QTimer::singleShot(1000, [this, spinner]() {
+            spinner->stop();
+            delete spinner;
+            ui->stackedWidget->setCurrentIndex(1);
         });
     });
 }
