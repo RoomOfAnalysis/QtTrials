8d804c3ae56b1e2d49d8999ebca978c6c323eb0d
Harold <pxduharold@126.com>
2024-05-07 11:43:57 +0800

    x save and restore scene rect to avoid scene moving

--- QtDrawOnVideo3/main.cpp
+++ QtDrawOnVideo3/main.cpp
@@ -37,12 +37,21 @@ int main(int argc, char* argv[])
     timer.setInterval(1000);
     // https://doc.qt.io/qt-5/graphicsview.html#the-graphics-view-coordinate-system
     QObject::connect(&timer, &QTimer::timeout, [&board]() {
-        auto [width, height] = board.size();
-        auto x = QRandomGenerator::global()->bounded(50, width - 50);
-        auto y = QRandomGenerator::global()->bounded(50, height - 80);
-        board.getScene()->addEllipse(x, y, 30, 30,
-                                     QPen(QColor(255, 255, 255, 255), 3, Qt::DashDotLine, Qt::RoundCap, Qt::RoundJoin),
+        for (auto item : board.getScene()->items())
+            if (item != board.getPixmapItem()) board.getScene()->removeItem(item);
+        auto [width, height] = board.getPixmapItem()->boundingRect().size().toSize();
+        int xx[] = {0, width};
+        int yy[] = {0, height};
+        auto x = xx[QRandomGenerator::global()->bounded(0, 2)];
+        auto y = yy[QRandomGenerator::global()->bounded(0, 2)];
+        // save and restore scene_rect to make sure adding item not moves the scene
+        auto scene_rect = board.getScene()->sceneRect();
+        board.getScene()->addEllipse(x, y, 30, 30, QPen(Qt::green, 3, Qt::DashDotLine, Qt::RoundCap, Qt::RoundJoin),
                                      QBrush(Qt::transparent));
+        auto* txt_item = board.getScene()->addText("hello world!");
+        txt_item->setDefaultTextColor(Qt::yellow);
+        txt_item->moveBy(x, y);
+        board.getScene()->setSceneRect(scene_rect);
     });
     timer.start();
 
