5854ee7b7aa3c261b024bf1e8b4ce7539ddd9fbd
Harold <pxduharold@126.com>
2024-05-29 16:14:01 +0800

    x update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,43 @@
 $mode = $args[0]
 $project_name = $args[1]
-$cur_dir = Get-Location
+$target_dir = "$(Get-Location)\build\$project_name"
+$qt_bin_dir = "C:\vcpkg\installed\x64-windows\tools\Qt6\bin"
+$qt_trans_dir = "C:\vcpkg\installed\x64-windows\translations"
+$qt_res_dir = "C:\vcpkg\installed\x64-windows\resources"
+$release_bin_dir = "C:\vcpkg\installed\x64-windows\bin"
+$debug_bin_dir = "C:\vcpkg\installed\x64-windows\debug\bin"
+$copy_from_bin = $debug_bin_dir
+$copy_to_bin = "$target_dir\Debug"
 
 if ($mode -eq "release")
 {
-    & "C:\vcpkg\installed\x64-windows\tools\Qt6\bin\windeployqt.exe" $cur_dir\build\$project_name\Release\$project_name.exe
+    & "$qt_bin_dir\windeployqt.exe" "$target_dir\Release\$project_name.exe"
+    $copy_from_bin = $release_bin_dir
+    $copy_to_bin = "$target_dir\Release"
 }
 else
 {
-    & "C:\vcpkg\installed\x64-windows\tools\Qt6\bin\windeployqt.debug.bat" $cur_dir\build\$project_name\Debug\$project_name.exe
+    & "$qt_bin_dir\windeployqt.debug.bat" "$target_dir\Debug\$project_name.exe"
 }
+
+# if with ffmpeg
+Copy-Item "$copy_from_bin\avcodec-60.dll" -Destination $copy_to_bin
+Copy-Item "$copy_from_bin\avdevice-60.dll" -Destination $copy_to_bin
+Copy-Item "$copy_from_bin\avfilter-9.dll" -Destination $copy_to_bin
+Copy-Item "$copy_from_bin\avformat-60.dll" -Destination $copy_to_bin
+Copy-Item "$copy_from_bin\avutil-58.dll" -Destination $copy_to_bin
+Copy-Item "$copy_from_bin\swresample-4.dll" -Destination $copy_to_bin
+Copy-Item "$copy_from_bin\swscale-7.dll" -Destination $copy_to_bin
+
+# if with sqlite3
+Copy-Item "$copy_from_bin\sqlite3.dll" -Destination $copy_to_bin
+
+# if with QtWebEngine
+Copy-Item -Path "$qt_trans_dir\*" -Destination "$copy_to_bin\translations" -Recurse
+Copy-Item -Path "$qt_res_dir\*" -Destination "$copy_to_bin\resources" -Recurse
+
+# if has translation file
+#Copy-Item "$target_dir\$project_name_zh_CN.qm" -Destination "$copy_to_bin\translations"
+
+# if has config.ini
+#Copy-Item "$target_dir\config.ini" -Destination $copy_to_bin
